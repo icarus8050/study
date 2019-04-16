90b58b38116bb6e2cf07bf1c37a9560cd155123f
leafy <icarus8050@naver.com>
2019-04-16 21:43:04 +0900

    project init

---
+++ mockitoedu/src/main/java/com/study/mockitoedu/MockitoeduApplication.java
@@ -0,0 +1,13 @@
+package com.study.mockitoedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MockitoeduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MockitoeduApplication.class, args);
+    }
+
+}
