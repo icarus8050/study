a4433fd975d15b2e4ef50bcf0e48e17b91a03514
leafy <icarus8050@naver.com>
2019-04-19 11:16:44 +0900

    init

---
+++ junit5withspringboot/src/main/java/com/example/junit5withspringboot/Junit5withspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.junit5withspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Junit5withspringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Junit5withspringbootApplication.class, args);
+    }
+
+}
---
+++ junit5withspringboot/src/test/java/com/example/junit5withspringboot/Junit5withspringbootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.junit5withspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Junit5withspringbootApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
