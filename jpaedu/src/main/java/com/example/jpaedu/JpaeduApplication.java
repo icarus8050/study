c99c0fe07acca851ab3feb2a70d0cb7758073193
leafy <icarus8050@naver.com>
2019-04-23 23:37:43 +0900

    init

---
+++ jpaedu/src/main/java/com/example/jpaedu/JpaeduApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaeduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaeduApplication.class, args);
+    }
+
+}
