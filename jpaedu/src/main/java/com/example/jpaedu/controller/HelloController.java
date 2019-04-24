f5240c061b5847e6aa440f49b97f93948a080a31
leafy <icarus8050@naver.com>
2019-04-25 00:26:24 +0900

    ResponseEntity exam

--- jpaedu/src/main/java/com/example/jpaedu/controller/HelloController.java
+++ jpaedu/src/main/java/com/example/jpaedu/controller/HelloController.java
@@ -1,5 +1,8 @@
 package com.example.jpaedu.controller;
 
+import com.example.jpaedu.domain.Member;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,5 +14,16 @@ public class HelloController {
     @ResponseBody
     public String hello() {
         return "hello";
+    }
+
+    @GetMapping("/hello2")
+    public ResponseEntity<Member> hello2() {
+        Member member = Member.builder()
+                .id(Long.valueOf(4))
+                .name("독일")
+                .age(Integer.valueOf(39))
+                .build();
+
+        return new ResponseEntity<>(member, HttpStatus.OK);
     }
 }
--- jpaedu/src/main/java/com/example/jpaedu/domain/Member.java
+++ jpaedu/src/main/java/com/example/jpaedu/domain/Member.java
@@ -1,6 +1,8 @@
 package com.example.jpaedu.domain;
 
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,10 +11,18 @@ import javax.persistence.Id;
 
 @Entity
 @Getter
+@NoArgsConstructor
 public class Member {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private Integer age;
+
+    @Builder
+    public Member(Long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
 }
