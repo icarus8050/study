84f0484debe8065f25b60f9484e18b6ad209ba67
leafy <icarus8050@naver.com>
2019-04-24 23:59:38 +0900

    sql 수정

--- jpaedu/src/main/resources/data-h2.sql
+++ jpaedu/src/main/resources/data-h2.sql
@@ -1,10 +1,5 @@
-CREATE TABLE member (
-    id LONG NOT NULL,
-    name VARCHAR(255),
-    age INTEGER NOT NULL,
-    PRIMARY KEY (id)
-);
 
-INSERT INTO member values ( 1, '대한민국', 26 );
-INSERT INTO member values ( 2, '미국', 28 );
-INSERT INTO member values ( 3, '프랑스', 23 );
+
+INSERT INTO member(id, name, age) values ( 1, '대한민국', 26 );
+INSERT INTO member (id, name, age) values ( 2, '미국', 28 );
+INSERT INTO member (id, name, age) values ( 3, '프랑스', 23 );
