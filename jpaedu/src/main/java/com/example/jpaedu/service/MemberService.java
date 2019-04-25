d3f57db725db12625b7e9d03d20c12655793dee2
leafy <icarus8050@naver.com>
2019-04-25 22:59:08 +0900

    jpa save exam

---
+++ jpaedu/src/main/java/com/example/jpaedu/service/MemberService.java
@@ -0,0 +1,20 @@
+package com.example.jpaedu.service;
+
+import com.example.jpaedu.domain.Member;
+import com.example.jpaedu.repository.MemberReposiroty;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberReposiroty memberReposiroty;
+
+    public Member signUp(String name, Integer age) {
+        Member memberInfo = Member.builder()
+                .name(name)
+                .age(age)
+                .build();
+        return memberReposiroty.save(memberInfo);
+    }
+}
