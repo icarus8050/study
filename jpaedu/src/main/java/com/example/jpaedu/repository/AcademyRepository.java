508fd443e565695593a490f55b234aef104961e1
leafy <icarus8050@naver.com>
2019-04-30 23:38:42 +0900

    AcademyRepository init

---
+++ jpaedu/src/main/java/com/example/jpaedu/repository/AcademyRepository.java
@@ -0,0 +1,7 @@
+package com.example.jpaedu.repository;
+
+import com.example.jpaedu.domain.Academy;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AcademyRepository extends JpaRepository<Academy, Long> {
+}
