d3cf5b1a5aca5fd0e5cac0a49eaef83221f1f059
DESKTOP-LGQIVAP\SSH <chul.yun@softwareinlife.com>
2019-05-02 18:09:51 +0900

    무한 루프 문제 코드

--- jpaedu/src/main/java/com/example/jpaedu/controller/TeamController.java
+++ jpaedu/src/main/java/com/example/jpaedu/controller/TeamController.java
@@ -1,8 +1,14 @@
 package com.example.jpaedu.controller;
 
+import com.example.jpaedu.domain.Member;
+import com.example.jpaedu.domain.Team;
 import com.example.jpaedu.service.TeamService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
@@ -10,5 +16,10 @@ public class TeamController {
 
     private final TeamService teamService;
 
-
+    @GetMapping("/findteam")
+    @ResponseBody
+    public List<Member> findteam(Long id) {
+        Team team = teamService.find(id);
+        return team.getMembers();
+    }
 }
