53cc01a8acdcd41079fd8911bfb539ef600186f0
DESKTOP-LGQIVAP\SSH <chul.yun@softwareinlife.com>
2019-05-02 17:13:18 +0900

    1 : N Entity 연습 코드

--- jpaedu/src/main/java/com/example/jpaedu/domain/Team.java
+++ jpaedu/src/main/java/com/example/jpaedu/domain/Team.java
@@ -3,10 +3,8 @@ package com.example.jpaedu.domain;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Getter
@@ -18,4 +16,7 @@ public class Team {
     private Long id;
 
     private String name;
+
+    @OneToMany(mappedBy = "team")
+    private List<Member> members;
 }
