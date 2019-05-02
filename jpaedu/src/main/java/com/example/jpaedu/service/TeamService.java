1d18be9cf85b01ad1ffc3d35e1b492c4cce5cfbd
DESKTOP-LGQIVAP\SSH <chul.yun@softwareinlife.com>
2019-05-02 17:30:27 +0900

    TeamEntity 찾기 기능 추가

--- jpaedu/src/main/java/com/example/jpaedu/service/TeamService.java
+++ jpaedu/src/main/java/com/example/jpaedu/service/TeamService.java
@@ -1,4 +1,16 @@
 package com.example.jpaedu.service;
 
+import com.example.jpaedu.domain.Team;
+import com.example.jpaedu.repository.TeamRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class TeamService {
+    private final TeamRepository teamRepository;
+
+    public Team find(Long id) {
+        return teamRepository.findById(id).orElse(null);
+    }
 }
