5dd4c3c35dcea970223cdf045465f42f90059e41
icarus8050 <icarus8050@naver.com>
2019-05-03 10:58:45 +0900

    빈 생성자와 Setter 없이도 property 주입이 되네??

--- jpaedu/src/main/java/com/example/jpaedu/service/TeamService.java
+++ jpaedu/src/main/java/com/example/jpaedu/service/TeamService.java
@@ -3,14 +3,18 @@ package com.example.jpaedu.service;
 import com.example.jpaedu.domain.Team;
 import com.example.jpaedu.repository.TeamRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TeamService {
     private final TeamRepository teamRepository;
 
     public Team find(Long id) {
-        return teamRepository.findById(id).orElse(null);
+        Team team = teamRepository.findById(id).orElse(null);
+        log.info("called find : " + team.getName());
+        return team;
     }
 }
