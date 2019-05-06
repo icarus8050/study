1058aebb0263107e2067c87f2fbcb0b48b581139
leafy <icarus8050@naver.com>
2019-05-06 18:14:15 +0900

    init

---
+++ jpamappingedu/src/main/java/com/example/jpamappingedu/JpamappingeduApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpamappingedu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpamappingeduApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpamappingeduApplication.class, args);
+    }
+
+}
