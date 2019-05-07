f2a4692e9cd862a2669844293f9006c2a6d072b4
leafy <icarus8050@naver.com>
2019-05-07 21:26:08 +0900

    데이터 추가

--- jpaedu/src/main/resources/data-h2_2.sql
+++ jpaedu/src/main/resources/data-h2_2.sql
@@ -1,5 +1,18 @@
 
 
-INSERT INTO member(name, age) values ('Korea', 26 );
-INSERT INTO member (name, age) values ('USA', 28 );
-INSERT INTO member (name, age) values ('France', 23 );
+INSERT INTO team (name) values ('인사팀');
+INSERT INTO team (name) values ('경영팀');
+INSERT INTO team (name) values ('개발팀');
+INSERT INTO team (name) values ('마케팅팀');
+INSERT INTO team (name) values ('고객지원팀');
+
+INSERT INTO member (name, age, team_id) values ('아이유', 26, 1);
+INSERT INTO member (name, age, team_id) values ('레드벨벳', 28, 2);
+INSERT INTO member (name, age, team_id) values ('비투비', 23, 2);
+INSERT INTO member (name, age, team_id) values ('악동뮤지션', 19, 4);
+INSERT INTO member (name, age, team_id) values ('헨리', 31, 3);
+INSERT INTO member (name, age, team_id) values ('씨야', 21, 5);
+INSERT INTO member (name, age, team_id) values ('엑소', 24, 1);
+INSERT INTO member (name, age, team_id) values ('마마무', 34, 5);
+INSERT INTO member (name, age, team_id) values ('윤종신', 42, 5);
+INSERT INTO member (name, age, team_id) values ('트와이스', 18, 3);
