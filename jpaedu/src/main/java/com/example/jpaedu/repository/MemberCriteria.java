f560d700c851fe09b1c71348b91b26502a4ea902
icarus8050 <icarus8050@naver.com>
2019-05-08 17:36:38 +0900

    Criteria 연습삼아..

---
+++ jpaedu/src/main/java/com/example/jpaedu/repository/MemberCriteria.java
@@ -0,0 +1,35 @@
+package com.example.jpaedu.repository;
+
+import com.example.jpaedu.domain.Member;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import javax.validation.constraints.NotNull;
+import java.util.List;
+
+@Repository
+public class MemberCriteria {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Transactional(readOnly = true)
+    public List<Member> findMember(@NotNull Pageable pageable) {
+        CriteriaBuilder builder = entityManager.getCriteriaBuilder();
+
+        //Select
+        CriteriaQuery<Member> query = builder.createQuery(Member.class);
+
+        //From
+        Root<Member> root = query.from(Member.class);
+
+        query.select(root);
+        return entityManager.createQuery(query).getResultList();
+    }
+}
