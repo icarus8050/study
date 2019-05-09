ce1bfa537b987bb5ef53c494d2e046364ac960eb
leafy <icarus8050@naver.com>
2019-05-09 23:33:19 +0900

    PagingService 구현

--- jpaedu/src/main/java/com/example/jpaedu/service/MemberService.java
+++ jpaedu/src/main/java/com/example/jpaedu/service/MemberService.java
@@ -3,6 +3,9 @@ package com.example.jpaedu.service;
 import com.example.jpaedu.domain.Member;
 import com.example.jpaedu.repository.MemberReposiroty;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,5 +27,12 @@ public class MemberService {
 
     public Member findByUserName(String name) {
         return memberReposiroty.findByUserName(name);
+    }
+
+    //PageRequest는 deprecated 되었다.
+    public Page<Member> findByPagingMember(Pageable pageable) {
+        pageable = PageRequest.of(pageable.getPageNumber() <= 0 ?
+                0 : pageable.getPageNumber() - 1, pageable.getPageSize());
+        return memberReposiroty.findAll(pageable);
     }
 }
