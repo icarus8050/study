540cb3067595be67975b6f485407b6e12e897f38
icarus8050 <icarus8050@naver.com>
2019-05-13 17:50:58 +0900

    간단한 예제 설정 파일 추가

--- junit5withspringboot/src/main/java/com/example/junit5withspringboot/configure/SampleConfig.java
+++ junit5withspringboot/src/main/java/com/example/junit5withspringboot/configure/SampleConfig.java
@@ -1,14 +1,17 @@
 package com.example.junit5withspringboot.configure;
 
 import com.example.junit5withspringboot.configure.bean.Calculator;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class SampleConfig {
 
     @Bean
     public Calculator calculator() {
+        log.info("calculator!");
         return new Calculator();
     }
 }
