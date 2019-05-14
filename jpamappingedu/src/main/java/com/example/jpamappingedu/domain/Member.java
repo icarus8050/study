43f9a89cdc4beb61441f0488f0c6ea7fd13e0aa5
leafy <icarus8050@naver.com>
2019-05-14 23:40:11 +0900

    init

---
+++ jpamappingedu/src/main/java/com/example/jpamappingedu/domain/Member.java
@@ -0,0 +1,30 @@
+package com.example.jpamappingedu.domain;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Member extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long memberId;
+
+    private String name;
+
+    private Long age;
+
+    @Builder
+    public Member(String name, Long age) {
+        this.name = name;
+        this.age = age;
+    }
+}
