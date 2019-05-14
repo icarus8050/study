aacf6501fd80acba0970641a68babc7772300c44
leafy <icarus8050@naver.com>
2019-05-15 00:09:42 +0900

    @CreatedBy에서 @CreatedDate로 어노테이션 수정

--- jpamappingedu/src/main/java/com/example/jpamappingedu/domain/BaseEntity.java
+++ jpamappingedu/src/main/java/com/example/jpamappingedu/domain/BaseEntity.java
@@ -2,6 +2,7 @@ package com.example.jpamappingedu.domain;
 
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
@@ -14,7 +15,7 @@ import java.time.LocalDateTime;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
 
-    @CreatedBy
+    @CreatedDate
     private LocalDateTime createdDate;
 
     @LastModifiedDate
