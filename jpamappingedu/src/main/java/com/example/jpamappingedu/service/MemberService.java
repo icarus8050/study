4e2d2e40135d42826ca681029b87ff3c4272aa34
icarus8050 <icarus8050@naver.com>
2019-05-17 16:50:58 +0900

    특정 나이 이하의 멤버 조회 서비스 구현

--- jpamappingedu/src/main/java/com/example/jpamappingedu/service/MemberService.java
+++ jpamappingedu/src/main/java/com/example/jpamappingedu/service/MemberService.java
@@ -1,15 +1,20 @@
 package com.example.jpamappingedu.service;
 
 import com.example.jpamappingedu.domain.Member;
+import com.example.jpamappingedu.repository.MemberCriteria;
 import com.example.jpamappingedu.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import javax.validation.constraints.NotNull;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final MemberCriteria memberCriteria;
 
     public Member findById(Long id) {
         return memberRepository.findById(id).orElse(null);
@@ -17,5 +22,9 @@ public class MemberService {
 
     public Member saveMember(Member member) {
         return memberRepository.save(member);
+    }
+
+    public List<Member> findMembersLessThanAge(@NotNull Long age) {
+        return memberCriteria.findMembersLessThanAge(age);
     }
 }
