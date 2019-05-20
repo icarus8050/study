285cbeae98a7e6b0de3bb0bebb2dfa2cb282af3f
icarus8050 <icarus8050@naver.com>
2019-05-20 15:39:37 +0900

    TeamRepository init

---
+++ jpamappingedu/src/main/java/com/example/jpamappingedu/repository/TeamRepository.java
@@ -0,0 +1,7 @@
+package com.example.jpamappingedu.repository;
+
+import com.example.jpamappingedu.controller.Team;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TeamRepository extends JpaRepository<Team, Long> {
+}
