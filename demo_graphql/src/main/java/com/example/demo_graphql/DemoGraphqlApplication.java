59064c93d2a0f79e9f7727f72cdde3483e3223b6
leafy <icarus8050@naver.com>
2019-05-28 23:56:48 +0900

    crud init

--- demo_graphql/src/main/java/com/example/demo_graphql/DemoGraphqlApplication.java
+++ demo_graphql/src/main/java/com/example/demo_graphql/DemoGraphqlApplication.java
@@ -1,13 +1,26 @@
 package com.example.demo_graphql;
 
+import com.example.demo_graphql.domain.Member;
+import com.example.demo_graphql.repository.MemberRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class DemoGraphqlApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DemoGraphqlApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner demo(MemberRepository memberRepository) {
+        return (args) -> {
+            Member member = Member.builder().name("aa").age(1L).build();
+            memberRepository.save(member);
+        };
+    }
 }
