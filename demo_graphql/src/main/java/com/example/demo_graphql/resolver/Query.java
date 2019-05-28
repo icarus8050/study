c99c24a76ceb6fb4a1878bfc309e96e346857c25
leafy <icarus8050@naver.com>
2019-05-28 23:56:56 +0900

    graphql init

---
+++ demo_graphql/src/main/java/com/example/demo_graphql/resolver/Query.java
@@ -0,0 +1,22 @@
+package com.example.demo_graphql.resolver;
+
+import com.coxautodev.graphql.tools.GraphQLQueryResolver;
+import com.example.demo_graphql.domain.Member;
+import com.example.demo_graphql.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class Query implements GraphQLQueryResolver {
+
+    private final MemberRepository memberRepository;
+
+    public Iterable<Member> findAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Long coutMembers() {
+        return memberRepository.count();
+    }
+}
