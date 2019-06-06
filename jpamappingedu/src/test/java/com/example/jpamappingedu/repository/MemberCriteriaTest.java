98ebee08cfa57d673f90a2f4ea1e69467c126b6e
leafy <icarus8050@naver.com>
2019-06-06 09:32:18 +0900

    새롭게 생성하여 저장한 Member 객체도 영속성 엔티티로 바뀌네?

--- jpamappingedu/src/test/java/com/example/jpamappingedu/repository/MemberCriteriaTest.java
+++ jpamappingedu/src/test/java/com/example/jpamappingedu/repository/MemberCriteriaTest.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -19,6 +18,9 @@ class MemberCriteriaTest {
     @Autowired
     MemberCriteria memberCriteria;
 
+    @Autowired
+    MemberRepository memberRepository;
+
     @Test
     void findAllTest() {
         List<Member> members = memberCriteria.findAll();
@@ -26,5 +28,22 @@ class MemberCriteriaTest {
         for (int i = 0; i < members.size(); i++) {
             log.info(members.get(i).getName());
         }
+    }
+
+    @Test
+    void repositorySaveTest() {
+        //member 객체도 영속성 객체로 변경이 되네?
+        Member member = Member.builder()
+                .name("하나")
+                .age(15L)
+                .build();
+
+        Member saved = memberRepository.save(member);
+
+        log.info(member.getMemberId().toString());
+        log.info(member.getClass().toString());
+        log.info(saved.getMemberId().toString());
+        System.out.println(member.equals(saved));
+        System.out.println(member == saved);
     }
 }
