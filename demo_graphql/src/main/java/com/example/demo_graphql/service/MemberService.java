61f39da6f7e0659dd61f73200e641674f8a78ef2
leafy <icarus8050@naver.com>
2019-06-11 23:15:16 +0900

    Service가 추가됨에 따라 GraphQLController 코드에도 변경이 이루어져야 하는 구조에 대해서 고민이 필요함

--- demo_graphql/src/main/java/com/example/demo_graphql/service/MemberService.java
+++ demo_graphql/src/main/java/com/example/demo_graphql/service/MemberService.java
@@ -5,6 +5,7 @@ import com.example.demo_graphql.repository.MemberRepository;
 import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLQuery;
+import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +16,7 @@ import java.util.List;
 @Service
 @Transactional
 @RequiredArgsConstructor
+@GraphQLApi
 public class MemberService {
 
     private final MemberRepository memberRepository;
