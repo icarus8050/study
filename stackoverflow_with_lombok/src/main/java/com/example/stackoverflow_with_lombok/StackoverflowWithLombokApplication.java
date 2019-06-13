28e543c1d96098f1af83719c7fc8f6da44a6e24f
leafy <icarus8050@naver.com>
2019-06-13 23:49:07 +0900

    project init

---
+++ stackoverflow_with_lombok/src/main/java/com/example/stackoverflow_with_lombok/StackoverflowWithLombokApplication.java
@@ -0,0 +1,13 @@
+package com.example.stackoverflow_with_lombok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StackoverflowWithLombokApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StackoverflowWithLombokApplication.class, args);
+    }
+
+}
---
+++ stackoverflow_with_lombok/src/test/java/com/example/stackoverflow_with_lombok/StackoverflowWithLombokApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.stackoverflow_with_lombok;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class StackoverflowWithLombokApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
