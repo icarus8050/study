df36245b2fbc09cabac00d68ed54b4f788146572
leafy <icarus8050@naver.com>
2019-06-17 01:35:47 +0900

    스프링 부트 환경 변수를 이용하여 외부 yml 설정 파일 적용하는 방법

--- enjoy/src/main/java/com/example/enjoy/config/SimpleConfig.java
+++ enjoy/src/main/java/com/example/enjoy/config/SimpleConfig.java
@@ -2,13 +2,8 @@ package com.example.enjoy.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-@PropertySources(
-        @PropertySource(value = "file:C:/Users/Leafy/Desktop/real_db.properties", ignoreResourceNotFound = true)
-)
 public class SimpleConfig {
 
     @Value("${aaa.bbb.ccc}")
