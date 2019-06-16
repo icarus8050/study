22bfcf26a4234a90d6097cc816dfd8f5d53d75ab
leafy <icarus8050@naver.com>
2019-06-17 02:11:16 +0900

    EnvironmentPostProcessor 이용 방법

--- enjoy/src/main/java/com/example/enjoy/EnjoyApplication.java
+++ enjoy/src/main/java/com/example/enjoy/EnjoyApplication.java
@@ -2,18 +2,12 @@ package com.example.enjoy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class EnjoyApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(EnjoyApplication.class)
-                .properties(
-                        "spring.config.location=" +
-                                "file:C:/Users/Leafy/Desktop/real_db.yml"
-                )
-                .run(args);
+        SpringApplication.run(EnjoyApplication.class, args);
     }
 
 }
