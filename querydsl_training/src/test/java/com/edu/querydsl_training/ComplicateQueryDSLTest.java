7962cf2ee076f03f70cc11f0ae4ad33ff7874ff7
icarus8050 <icarus8050@naver.com>
2019-07-12 23:53:48 +0900

    offset, limit 사용해보기

---
+++ querydsl_training/src/test/java/com/edu/querydsl_training/ComplicateQueryDSLTest.java
@@ -0,0 +1,65 @@
+package com.edu.querydsl_training;
+
+import com.edu.querydsl_training.domain.Member;
+import com.edu.querydsl_training.domain.QMember;
+import com.edu.querydsl_training.domain.QTeam;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+public class ComplicateQueryDSLTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    JPAQueryFactory query;
+
+    @Before
+    public void setUp() {
+        query = new JPAQueryFactory(em);
+    }
+
+    @Test
+    public void pagingAndOrderTest() {
+        QMember m = QMember.member;
+
+        List<Member> memberList = query.selectFrom(m)
+                .where(m.age.loe(30))
+                .orderBy(m.memberId.asc())
+                .offset(5).limit(5)
+                .fetch();
+
+        for (Member member : memberList) {
+            log.info("member Id : " + member.getMemberId());
+            log.info("member name : " + member.getName());
+            log.info("member age : " + member.getAge());
+        }
+    }
+
+    @Test
+    public void groupByTest() {
+        QMember m = QMember.member;
+        QTeam t = QTeam.team;
+
+        List<Member> members = query.selectFrom(m)
+                .innerJoin(m.team, t).fetchJoin()
+                .groupBy(t)
+                .having(t.awardCount.goe(5))
+                .fetch();
+
+        for (Member member : members) {
+            log.info(member.getName());
+        }
+    }
+}
