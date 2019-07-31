1ab34afe5a38fe8e54d81efa243e0973cdae78c5
icarus8050 <icarus8050@naver.com>
2019-07-31 23:47:08 +0900

    컴포지션을 이용한 확장 예제 코드

--- effectivejava3rd/src/test/java/item_18/InstrumentedHashSetTest.java
+++ effectivejava3rd/src/test/java/item_18/InstrumentedHashSetTest.java
@@ -3,6 +3,7 @@ package item_18;
 import org.junit.Test;
 
 import java.util.Arrays;
+import java.util.TreeSet;
 
 import static org.junit.Assert.*;
 
@@ -13,6 +14,16 @@ public class InstrumentedHashSetTest {
         InstrumentedHashSet<String> s = new InstrumentedHashSet<>();
         s.addAll(Arrays.asList("하나", "둘", "셋"));
 
+        System.out.println("this count is : " + s.getAddCount());
+    }
+
+    @Test
+    public void 컴포지션을_활용한_예제() {
+        InstrumentedSet<String> s = new InstrumentedSet<>(new TreeSet<>());
+        s.add("하나");
+        s.add("둘");
+        s.add("셋");
+        s.addAll(Arrays.asList("넷", "다섯", "여섯"));
         System.out.println("this count is : " + s.getAddCount());
     }
 
