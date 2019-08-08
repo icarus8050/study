a4009717adee179d6269ed69c3b3f4d423ac422c
icarus8050 <icarus8050@naver.com>
2019-08-09 00:03:30 +0900

    item_29 제네릭 기반 스택 두 번째 방법

---
+++ effectivejava3rd/src/main/java/item_29/StackGeneric_2.java
@@ -0,0 +1,40 @@
+package item_29;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+public class StackGeneric_2<E> {
+    private Object[] elements;
+    private int size = 0;
+    private static final int DEFAULT_INITIAL_CAPACITY = 16;
+
+    public StackGeneric_2() {
+        elements = new Object[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    public void push(E e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public E pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+
+        @SuppressWarnings("unchecked")
+        E result = (E) elements[--size];
+        elements[size] = null;
+        return result;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    private void ensureCapacity() {
+        if (elements.length == size) {
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+}
