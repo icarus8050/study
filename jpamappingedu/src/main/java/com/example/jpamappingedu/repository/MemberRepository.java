f249036de7aaad789f1b714a59181a203775fdf6
icarus8050 <icarus8050@naver.com>
2019-08-21 22:58:55 +0900

    삭제..

--- jpamappingedu/src/main/java/com/example/jpamappingedu/repository/MemberRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpamappingedu.repository;
-
-import com.example.jpamappingedu.domain.Member;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MemberRepository extends JpaRepository<Member, Long> {
-}
--- jpamappingedu/src/main/java/com/example/jpamappingedu/repository/TeamRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.jpamappingedu.repository;
-
-import com.example.jpamappingedu.domain.Team;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TeamRepository extends JpaRepository<Team, Long> {
-}
