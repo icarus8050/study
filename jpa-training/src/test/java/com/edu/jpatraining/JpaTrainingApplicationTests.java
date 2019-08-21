68d48f241d7c83dfafea15def2e47f0b7e327978
icarus8050 <icarus8050@naver.com>
2019-08-21 23:01:00 +0900

    init

---
+++ jpa-training/src/test/java/com/edu/jpatraining/JpaTrainingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.edu.jpatraining;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JpaTrainingApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
