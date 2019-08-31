e5424a2de9518fcfd9d110a1db598bdbdf76c363
icarus8050 <icarus8050@naver.com>
2019-08-31 23:49:41 +0900

    타입 안전 이종 컨테이너 exam

---
+++ effectivejava3rd/src/main/java/item_33/FavoritesMain.java
@@ -0,0 +1,18 @@
+package item_33;
+
+public class FavoritesMain {
+
+    public static void main(String[] args) {
+        Favorites favorites = new Favorites();
+
+        favorites.putFavorite(String.class, "Java");
+        favorites.putFavorite(Integer.class, 123);
+        favorites.putFavorite(Class.class, Favorites.class);
+
+        String favoriteString = favorites.getFavorite(String.class);
+        int favoriteInt = favorites.getFavorite(Integer.class);
+        Class<?> favoriteClass = favorites.getFavorite(Class.class);
+
+        System.out.printf("%s %d %s%n", favoriteString, favoriteInt, favoriteClass.getName());
+    }
+}
