d56df8098347db3a09319b315f3cec28bd98bcac
icarus8050 <icarus8050@naver.com>
2019-09-04 23:41:20 +0900

    init

---
+++ transactional-ex/src/main/java/com/example/transactionalex/TransactionalExApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionalex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionalExApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionalExApplication.class, args);
+    }
+
+}
